1981bcbbcdfa641e769219f872667de8783bc507
fataneh-mow <fataneh.mow@gmail.com>
2026-08-02 18:49:32 +0430

    badge created

--- kaar-yab/components/common/index.ts
+++ kaar-yab/components/common/index.ts
@@ -4,3 +4,4 @@ export { default as Input } from "./Input";
 export { default as Modal } from "./Modal";
 export { default as Card } from "./Card";
 export { default as LoadingSkeleton } from "./LoadingSkeleton";
+export { default as Badge } from "./Badge";

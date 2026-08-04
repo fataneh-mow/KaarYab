89f7b0509927f553cbf3cffe60756639fad01f75
fataneh-mow <fataneh.mow@gmail.com>
2026-08-04 18:48:32 +0430

    home page refactored

--- kaar-yab/app/(public)/about/page.tsx
+++ kaar-yab/app/(public)/about/page.tsx
@@ -2,8 +2,6 @@ import AboutHero from "@/components/about/AboutHero";
 import AboutMission from "@/components/about/AboutMission";
 import AboutTargetUsers from "@/components/about/AboutTargetUsers";
 import AboutValues from "@/components/about/AboutValues";
-import AboutCTA from "@/components/about/AboutCTA";
-
 
 export default function AboutPage() {
 
@@ -13,7 +11,6 @@ export default function AboutPage() {
             <AboutMission />
             <AboutTargetUsers />
             <AboutValues />
-            <AboutCTA />
         </main>
     );
 

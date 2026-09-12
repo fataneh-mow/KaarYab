30c2b27b2bb03d47c1db5815c6ec2a19528dd249
fataneh-mow <fataneh.mow@gmail.com>
2026-09-12 12:40:03 +0430

    testing change added to the project

--- kaar-yab/components/dashboard/my-opportunities/EmptyState.tsx
+++ kaar-yab/components/dashboard/my-opportunities/EmptyState.tsx
@@ -27,7 +27,7 @@ export default function EmptyState(){
 
             <h2 className="mt-5 text-2xl font-bold text-slate-900 dark:text-white">
 
-                No opportunities yet
+                No opportunities yet, this is for testing added some new things
 
             </h2>
 
